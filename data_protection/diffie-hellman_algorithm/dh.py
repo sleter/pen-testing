adf41064bb3ab3bd6576d232b46dc7402656a7c1
Szymon Janowski <sleter97@gmail.com>
2018-11-24 16:46:19 +0100

    Update dh.py

--- data_protection/diffie-hellman_algorithm/dh.py
+++ data_protection/diffie-hellman_algorithm/dh.py
@@ -28,6 +28,7 @@ class DH:
             if(probablyPrime(n, accuracy=100)): return n
             else: n+=1
 
+    # https://stackoverflow.com/questions/40190849/efficient-finding-primitive-roots-modulo-n-using-python
     def primitiveRoots(self, modulo):
         coprime_set = {num for num in range(1, modulo) if gcd(num, modulo) == 1}
         return [g for g in range(1, modulo) if coprime_set == {pow(g, powers, modulo) for powers in range(1, modulo)}]
