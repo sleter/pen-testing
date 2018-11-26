230cb03e42d94e458ee8547a0aacd2e3387ff79b
sleter <sleter97@gmail.com>
2018-11-26 22:29:43 +0100

    LSB stenography

--- data_protection/steganography_LSB/simple_steganography.py
+++ data_protection/steganography_LSB/simple_steganography.py
@@ -12,21 +12,16 @@ class LSBSteg:
         # B = image.split()[2]
         text = ''
         binText=''
-        counter = 1
+        counter = 0
         for i in range(image.size[0]):
             for j in range(image.size[1]):
-                if counter == self.textLen:
-                    break
-                else:                 
-                    if counter % 8== 0 and counter != 0:
-                        # print(binText)
-                        text += self.binary_to_text(binText)
-                        binText = ''
-                    else:
-                        # print(bin(R.getpixel((i, j))))
-                        binText += str(bin(R.getpixel((i, j))))[-1]
+                if counter < self.textLen:
+                    binText += bin(R.getpixel((i, j)))[-1]
+                    print(bin(R.getpixel((i, j))))
                     counter+=1
-        print(text)
+                else:                 
+                    break
+        print(binText)
 
         
     def encode_image(self, pathToImage, text, pathToSave):
@@ -37,35 +32,33 @@ class LSBSteg:
         encoded_image = Image.new("RGB", image.size)
         pixels = encoded_image.load()
         text = self.text_to_binary(text)
+        # print(text)
         self.textLen = len(text)
         for i in range(image.size[0]):
             for j in range(image.size[1]):
-                if not text or len(text) == 1:
+                if not text:
                     pixels[i, j] = (R.getpixel((i, j)), G.getpixel((i, j)), B.getpixel((i, j)))
                 else:
-                    redPix = int(str(bin(R.getpixel((i, j))))[:-1] + text[0], 2)
-                    print(str(bin(R.getpixel((i, j))))[:-1] + text[0])
-                    print(bin(R.getpixel((i, j))))
+                    redPix = int(bin(R.getpixel((i, j)))[:-1] + text[0], 2)
+                    # print(str(bin(R.getpixel((i, j)))) + text[0])
                     # greenPix = int(str(bin(G.getpixel((i, j)))[2:].zfill(8))[:-1] + text[1], 2)
                     # bluePix = int(str(bin(B.getpixel((i, j)))[2:].zfill(8))[:-1] + text[2], 2)
-                    pixels[i, j] = (redPix, G.getpixel((i, j)), B.getpixel((i, j)))
-                    text = text[1:]
-                    
+                    pixels[i, j] = (254, G.getpixel((i, j)), B.getpixel((i, j)))
+                    text = text[1:]  
         encoded_image.save(pathToSave+'encoded_image.jpg')
 
     def text_to_binary(self, text):
         return ''.join(format(ord(x), 'b') for x in text)
     
     def binary_to_text(self, binary):
-        # print(str(int('0b'+binary, 2)))
         return str(chr(int('0b'+binary, 2)))
 
 
 
 def main():
     l = LSBSteg()
-    l.encode_image('/home/sleter/Pictures/Wallpapers/tvAVMw5.jpg', 'abcde', '/home/sleter/Pictures/')
-    print(l.decode_image('/home/sleter/Pictures/encoded_image.jpg'))
+    l.encode_image('/home/sleter/Pictures/Wallpapers/tvAVMw5.jpg', '1', '/home/sleter/Pictures/')
+    l.decode_image('/home/sleter/Pictures/encoded_image.jpg')
 
 if __name__ == '__main__':
     main()
