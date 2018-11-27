4e3e598991c88d8017117da1b9bbc44ba39a7452
sleter <sleter97@gmail.com>
2018-11-27 16:39:38 +0100

    RGB LSB

--- data_protection/steganography_LSB/simple_steganography.py
+++ data_protection/steganography_LSB/simple_steganography.py
@@ -2,26 +2,31 @@ import os, textwrap, binascii
 from PIL import Image, ImageFont, ImageDraw
 
 class LSBSteg:
-    def __init__(self):
+    def __init__(self, RC, GC, BC):
         self.textLen = 0
+        self.RC = RC
+        self.GC = GC
+        self.BC = BC
 
     def decode_image(self, pathToImage):
         image = Image.open(pathToImage)
         # image = image.convert('RGB')
         R = image.split()[0]
-        # G = image.split()[1]
-        # B = image.split()[2]
+        G = image.split()[1]
+        B = image.split()[2]
         binText=''
         counter = 0
         for i in range(image.size[0]):
-            for j in range(image.size[1]):
-                if counter < self.textLen:
-                    binText += bin(R.getpixel((i, j)))[-1]
-                    # print(int(bin(R.getpixel((i, j))), 2))
-                    counter+=1
-                else:                 
-                    break
-        print(binText)
+            for j in range(image.size[1]):                
+                binText += bin(R.getpixel((i, j)))[-self.RC:]
+                binText += bin(G.getpixel((i, j)))[-self.GC:]
+                binText += bin(B.getpixel((i, j)))[-self.BC:]
+                   
+        # print(self.textLen)
+        binText = binText[:self.textLen]
+
+        # print(binText)
+        print(self.binary_to_text(binText))
 
         
     def encode_image(self, pathToImage, text, pathToSave):
@@ -32,35 +37,49 @@ class LSBSteg:
         B = image.split()[2]
         encoded_image = Image.new("RGB", image.size)
         pixels = encoded_image.load()
+        self.textLen = len(text)*6
         text = self.text_to_binary(text)
         # print(text)
-        self.textLen = len(text)
+        # print(self.binary_to_text(text))
+        
         for i in range(image.size[0]):
             for j in range(image.size[1]):
                 if not text:
                     pixels[i, j] = (R.getpixel((i, j)), G.getpixel((i, j)), B.getpixel((i, j)))
                 else:
-                    redPix = int(bin(R.getpixel((i, j)))[:-1] + text[0], 2)
-                    # print(str(bin(R.getpixel((i, j)))) + text[0])
-                    # greenPix = int(str(bin(G.getpixel((i, j)))[2:].zfill(8))[:-1] + text[1], 2)
-                    # bluePix = int(str(bin(B.getpixel((i, j)))[2:].zfill(8))[:-1] + text[2], 2)
-                    pixels[i, j] = (redPix, G.getpixel((i, j)), B.getpixel((i, j)))
-                    text = text[1:]  
+                    redPix = int(bin(R.getpixel((i, j)))[:-self.RC] + text[:self.RC], 2)
+                    text = text[self.RC:]
+                    greenPix = int(bin(G.getpixel((i, j)))[:-self.GC] + text[:self.GC], 2)
+                    text = text[self.GC:]
+                    bluePix = int(bin(B.getpixel((i, j)))[:-self.BC] + text[:self.BC], 2)
+                    text = text[self.BC:]
+                    pixels[i, j] = (redPix, greenPix, bluePix)
+                    
         encoded_image.save(pathToSave+'encoded_image.png')
 
     def text_to_binary(self, text):
         return ''.join(format(ord(x), 'b') for x in text)
     
     def binary_to_text(self, binary):
-        return str(chr(int('0b'+binary, 2)))
-
-
+        pom, text = '', ''
+        binary += ' '
+        # print("binary:"+binary)
+        for i in binary:
+            if len(pom) % 6 == 0 and len(pom) != 0:
+                text += str(chr(int(str('0b'+pom), 2)))
+                # print(text)
+                # print(pom)
+                pom=i
+            else:
+                 pom += i
+                #  print(pom)
+        return text
 
 def main():
-    l = LSBSteg()
-    l.encode_image('/home/sleter/Pictures/Wallpapers/tvAVMw5.jpg', '123', '/home/sleter/Pictures/')
+    l = LSBSteg(4,4,4)
+    input_str = '542 \"0\"123'*100
+    l.encode_image('/home/sleter/Pictures/Wallpapers/tvAVMw5.jpg', input_str ,'/home/sleter/Pictures/')
     l.decode_image('/home/sleter/Pictures/encoded_image.png')
 
 if __name__ == '__main__':
     main()
-    
